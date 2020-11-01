3ba6af31c36d2374d8a5173c378894d82e3860c9
Tesar Gantara Suherman <tgantara@gmail.com>
2020-11-01 18:05:41 +0700

    update kategori

--- src/App.js
+++ src/App.js
@@ -12,11 +12,12 @@ export default class App extends Component {
 
     this.state = {
       menus: [],
+      kategoriYangDipilih : "Makanan"
     }
   }
 
   componentDidMount() {
-    Axios.get(API_URL + 'products').then(res => {
+    Axios.get(API_URL + 'products?category.nama='+ this.state.kategoriYangDipilih).then(res => {
       this.setState({
         menus: res.data
       })
@@ -25,16 +26,30 @@ export default class App extends Component {
         console.log(error)
       })
   }
+  gantiKategori = (value)=>{
+    this.setState({
+        kategoriYangDipilih : value,
+        menus : []
+    })
+    Axios.get(API_URL + 'products?category.nama='+ value).then(res => {
+      this.setState({
+        menus: res.data
+      })
+    })
+      .catch(error => {
+        console.log(error)
+      })
 
+}
   render() {
-    const {menus} = this.state
+    const {menus,kategoriYangDipilih} = this.state
     return (
       <Fragment>
         <Navbar />
         <div className="mt-2">
           <div className="container">
             <div className="row">
-              <ListCategory />
+              <ListCategory gantiKategori={this.gantiKategori } kategoriYangDipilih ={kategoriYangDipilih}/>
               <div className="col mt-2">
                 <h4><strong> Daftar Produk</strong></h4>
                 <hr />
--- src/components/ListCategory.jsx
+++ src/components/ListCategory.jsx
@@ -2,6 +2,20 @@ import Axios from 'axios';
 import React, { Component } from 'react'
 import { API_URL } from '../utils/URL';
 
+
+const Icon = ({ nama }) => {
+    if (nama == "Makanan") {
+        return (
+            <span class="lnr lnr-dinner"></span>
+        )
+    }
+    if (nama == "Minuman") {
+        return <span class="lnr lnr-coffee-cup"></span>
+    }
+    else {
+        return <span class="lnr lnr-dinner"></span>
+    }
+}
 export default class ListCategory extends Component {
     constructor(props) {
         super(props)
@@ -22,16 +36,21 @@ export default class ListCategory extends Component {
 
     }
 
+
+
     render() {
-        const {categories} = this.state
+        const { categories } = this.state
+        const { gantiKategori, kategoriYangDipilih } = this.props
         return (
             <div className="col-md-2 mt-2">
                 <h5><strong>Daftar Ketgori</strong></h5>
                 <hr />
-                <ul className="list-group">
+                <ul className="list-group" >
                     {
-                        categories && categories.map((category)=>{
-                        return  <li className="list-group-item" key={category.id}>{category.nama} <span class="lnr lnr-dinner"></span> </li>
+                        categories && categories.map((category) => {
+                            return (
+                                <li className="list-group-item" key={category.id} onClick={() => gantiKategori(category.nama)}><Icon nama={category.nama} />{category.nama}  </li>
+                            )
                         })
                     }
                 </ul>
