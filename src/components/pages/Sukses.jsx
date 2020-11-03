6e1ce1499b59d3976e99fb3617e9fd7760b530af
Tesar Gantara Suherman <tgantara@gmail.com>
2020-11-03 14:07:46 +0700

    udpate

--- src/components/pages/Sukses.jsx
+++ src/components/pages/Sukses.jsx
@@ -5,6 +5,7 @@ export default class Sukses extends Component {
     render() {
         return (
             <div className="mt-4 text-center">
+                <img src="assets/images/success.png" width={500}/>
                <h2>Pesanan Sukses</h2>
                 <p>Terimakasih Sudah Memesan</p>
                 <Link to="/" className="btn btn-info">Kembali</Link>
